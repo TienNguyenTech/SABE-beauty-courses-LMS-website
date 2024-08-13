d09517bb7f86f0c6d021bc7a4e0d586a54d7b239
Surya Vamshi Mukunda Rao <srao0005@student.monash.edu>
2024-08-13 17:25:44 +1000

    Fixed Footer issue, added testimonials.

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
 
     <!-- title -->
-    <title>SABE</title>
+    <title>SABE | South Adelaide Beauty and Education</title>
 
     <!-- favicon -->
     <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
@@ -32,11 +32,6 @@
     <link rel="stylesheet" href="assets/css/responsive.css">
 
 </head>
-<style>
-    body {
-        background-color: #74C69D; /* Light green background color for the entire page */
-    }
-</style>
 <body>
 
 <!--PreLoader-->
@@ -143,7 +138,7 @@
                 <div class="col-md-12 col-lg-7 offset-lg-1 offset-xl-0">
                     <div class="hero-text">
                         <div class="hero-text-tablecell">
-                            <h1>Shape Your Future in Beauty</h1>
+                            <h1>Shape Your Future In Beauty</h1>
                             <div class="hero-btns">
                                 <a href="shop.html" class="boxed-btn">Our Services</a>
                                 <a href="contact.html" class="bordered-btn">Contact Us</a>
@@ -162,6 +157,7 @@
                     <div class="hero-text">
                         <div class="hero-text-tablecell">
                             <h1>Where Creativity Meets Skill</h1>
+
                             <div class="hero-btns">
                                 <a href="shop.html" class="boxed-btn">Visit Shop</a>
                                 <a href="contact.html" class="bordered-btn">Contact Us</a>
@@ -204,8 +200,8 @@
                         <i class="fas fa-shipping-fast"></i>
                     </div>
                     <div class="content">
-                        <h3>Expert Instructors</h3>
-                        <p>Learn from seasoned professionals in the industry. </p>
+                        <h3>International Expert Instructors</h3>
+                        <p>Learn from seasoned professionals in the industry</p>
                     </div>
                 </div>
             </div>
@@ -215,7 +211,7 @@
                         <i class="fas fa-phone-volume"></i>
                     </div>
                     <div class="content">
-                        <h3>Flexible Schedules</h3>
+                        <h3>Flexible Classes</h3>
                         <p>Day, evening, and weekend classes available.</p>
                     </div>
                 </div>
@@ -243,12 +239,12 @@
         <div class="row">
             <div class="col-lg-8 offset-lg-2 text-center">
                 <div class="section-title">
-                    <h3><span class="orange-text">Our</span> Programs</h3>
-                    <p>Explore our diverse range of programs designed to elevate your skills and prepare you for a successful career in the beauty industry.</p>
+                    <h3><span class="orange-text">Featured</span> Courses</h3>
+                    <p>>Explore our diverse range of programs designed to elevate your skills and prepare you for a successful career in the beauty industry.</p>
                 </div>
             </div>
         </div>
-        <!-- HTML -->
+
         <div class="row">
             <div class="col-lg-4 col-md-6 text-center">
                 <div class="single-product-item">
@@ -257,7 +253,6 @@
                     </div>
                     <h3>Back to Basics: Facials</h3>
                     <p class="product-price"><span>Learn the Fundamentals<br> of performing Facials</span> 199$ </p>
-                    <!--<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>-->
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 text-center">
@@ -267,7 +262,6 @@
                     </div>
                     <h3>Back to Basics: Lash & Brow </h3>
                     <p class="product-price"><span>Master the techniques needed <br> to elevate your lash and brow artistry </span> 199$ </p>
-                    <!--<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>-->
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0 text-center">
@@ -276,65 +270,15 @@
                         <a href="single-product.html"><img src="assets/img/products/product-img-3.jpg" alt=""></a>
                     </div>
                     <h3>Total Care: Waxing Essentials</h3>
-                    <p class="product-price"><span>Precision and Expertise: Achieve Waxing excellence</span> 499$ </p>
-                    <!--<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>-->
+                    <p class="product-price"><span>Take your Waxing skills to the next level <br> and impress your clients </span> 99$ </p>
                 </div>
-            </div>
-        </div>
-
-        <!-- CSS -->
-        <style>
-            .product-image {
-                width: 100%;
-                padding-top: 100%;
-                position: relative;
-                overflow: hidden;
-            }
-
-            .product-image img {
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
-        </style>
-
-<!-- end product section -->
-
-<!-- cart banner section -->
-<section class="cart-banner pt-100 pb-100">
-    <div class="container">
-        <div class="row clearfix">
-            <!--Image Column-->
-            <div class="image-column col-lg-6">
-                <div class="image">
-                    <div class="price-box">
-                        <div class="inner-price">
-                                <span class="price">
-                                    <strong>New!</strong> <br> Custom plan
-                                </span>
-                        </div>
-                    </div>
-                    <img src="assets/img/a.jpg" alt="">
-                </div>
-            </div>
-            <!--Content Column-->
-            <div class="content-column col-lg-6">
-                <h3><span class="orange-text">Personalised </span> Courses</h3>
-                <h4>A teaching experience as unique as every student</h4>
-                <div class="text">Experience a training program that is designed to enhance your natural strengths, whole improving any upon pre-existing skills and talents.</div>
-                <!--Countdown Timer-->
-                <div class="time-counter"><div class="time-countdown clearfix" data-countdown="2024/9/01"><div class="counter-column"><div class="inner"><span class="count">00</span>Days</div></div> <div class="counter-column"><div class="inner"><span class="count">00</span>Hours</div></div>  <div class="counter-column"><div class="inner"><span class="count">00</span>Mins</div></div>  <div class="counter-column"><div class="inner"><span class="count">00</span>Secs</div></div></div></div>
-                <!--<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>-->
             </div>
         </div>
     </div>
-</section>
-<!-- end cart banner section -->
+</div>
+<!-- end product section -->
 
-<!-- testimonail-section -->
+<!-- testimonial-section -->
 <div class="testimonail-section mt-150 mb-150">
     <div class="container">
         <div class="row">
@@ -345,9 +289,9 @@
                             <img src="assets/img/avaters/avatar1.png" alt="">
                         </div>
                         <div class="client-meta">
-                            <h3>Saira Hakim <span>Local shop owner</span></h3>
+                            <h3>Saira Hakim <span>Course: Back to Basics (Facial)</span></h3>
                             <p class="testimonial-body">
-                                " Sed ut perspiciatis unde omnis iste natus error veritatis et  quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium "
+                                "Attending SABE was a game-changer for my career. The hands-on training and the guidance from industry professionals helped me refine my skills and build a strong portfolio "
                             </p>
                             <div class="last-icon">
                                 <i class="fas fa-quote-right"></i>
@@ -359,10 +303,9 @@
                             <img src="assets/img/avaters/avatar2.png" alt="">
                         </div>
                         <div class="client-meta">
-                            <h3>David Niph <span>Local shop owner</span></h3>
+                            <h3>Sarah Lee <span>Course: Personalised Training</span></h3>
                             <p class="testimonial-body">
-                                " Sed ut perspiciatis unde omnis iste natus error veritatis et  quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium "
-                            </p>
+                                "I can’t thank SABE enough for the transformative education I received. The school not only taught me the technical skills needed to excel in esthetics but also provided me with a deep understanding of client care"                            </p>
                             <div class="last-icon">
                                 <i class="fas fa-quote-right"></i>
                             </div>
@@ -373,9 +316,9 @@
                             <img src="assets/img/avaters/avatar3.png" alt="">
                         </div>
                         <div class="client-meta">
-                            <h3>Jacob Sikim <span>Local shop owner</span></h3>
+                            <h3>Laura Smith <span>Course: Total Care (Waxing)</span></h3>
                             <p class="testimonial-body">
-                                " Sed ut perspiciatis unde omnis iste natus error veritatis et  quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium "
+                                "The comprehensive training at SABE gave me a strong foundation in all aspects of Waxing. From basic techniques to advanced procedures, the curriculum was well-rounded and thorough"
                             </p>
                             <div class="last-icon">
                                 <i class="fas fa-quote-right"></i>
@@ -387,134 +330,8 @@
         </div>
     </div>
 </div>
-<!-- end testimonail-section -->
+<!-- end testimonial-section -->
 
-<!-- advertisement section -->
-<div class="abt-section mb-150">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-12">
-                <div class="abt-bg">
-                    <a href="https://www.youtube.com/watch?v=DBLlFWYcIGQ" class="video-play-btn popup-youtube"><i class="fas fa-play"></i></a>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-12">
-                <div class="abt-text">
-                    <p class="top-sub">Since Year 1999</p>
-                    <h2>We are <span class="orange-text">Fruitkha</span></h2>
-                    <p>Etiam vulputate ut augue vel sodales. In sollicitudin neque et massa porttitor vestibulum ac vel nisi. Vestibulum placerat eget dolor sit amet posuere. In ut dolor aliquet, aliquet sapien sed, interdum velit. Nam eu molestie lorem.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente facilis illo repellat veritatis minus, et labore minima mollitia qui ducimus.</p>
-                    <a href="about.html" class="boxed-btn mt-4">know more</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end advertisement section -->
-
-<!-- shop banner -->
-<section class="shop-banner">
-    <div class="container">
-        <h3>December sale is on! <br> with big <span class="orange-text">Discount...</span></h3>
-        <div class="sale-percent"><span>Sale! <br> Upto</span>50% <span>off</span></div>
-        <a href="shop.html" class="cart-btn btn-lg">Shop Now</a>
-    </div>
-</section>
-<!-- end shop banner -->
-
-<!-- latest news -->
-<div class="latest-news pt-150 pb-150">
-    <div class="container">
-
-        <div class="row">
-            <div class="col-lg-8 offset-lg-2 text-center">
-                <div class="section-title">
-                    <h3><span class="orange-text">Our</span> News</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-4 col-md-6">
-                <div class="single-latest-news">
-                    <a href="single-news.html"><div class="latest-news-bg news-bg-1"></div></a>
-                    <div class="news-text-box">
-                        <h3><a href="single-news.html">You will vainly look for fruit on it in autumn.</a></h3>
-                        <p class="blog-meta">
-                            <span class="author"><i class="fas fa-user"></i> Admin</span>
-                            <span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
-                        </p>
-                        <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
-                        <a href="single-news.html" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="single-latest-news">
-                    <a href="single-news.html"><div class="latest-news-bg news-bg-2"></div></a>
-                    <div class="news-text-box">
-                        <h3><a href="single-news.html">A man's worth has its season, like tomato.</a></h3>
-                        <p class="blog-meta">
-                            <span class="author"><i class="fas fa-user"></i> Admin</span>
-                            <span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
-                        </p>
-                        <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
-                        <a href="single-news.html" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
-                <div class="single-latest-news">
-                    <a href="single-news.html"><div class="latest-news-bg news-bg-3"></div></a>
-                    <div class="news-text-box">
-                        <h3><a href="single-news.html">Good thoughts bear good fresh juicy fruit.</a></h3>
-                        <p class="blog-meta">
-                            <span class="author"><i class="fas fa-user"></i> Admin</span>
-                            <span class="date"><i class="fas fa-calendar"></i> 27 December, 2019</span>
-                        </p>
-                        <p class="excerpt">Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.</p>
-                        <a href="single-news.html" class="read-more-btn">read more <i class="fas fa-angle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <a href="news.html" class="boxed-btn">More News</a>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end latest news -->
-
-<!-- logo carousel -->
-<div class="logo-carousel-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="logo-carousel-inner">
-                    <div class="single-logo-item">
-                        <img src="assets/img/company-logos/1.png" alt="">
-                    </div>
-                    <div class="single-logo-item">
-                        <img src="assets/img/company-logos/2.png" alt="">
-                    </div>
-                    <div class="single-logo-item">
-                        <img src="assets/img/company-logos/3.png" alt="">
-                    </div>
-                    <div class="single-logo-item">
-                        <img src="assets/img/company-logos/4.png" alt="">
-                    </div>
-                    <div class="single-logo-item">
-                        <img src="assets/img/company-logos/5.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end logo carousel -->
 
 <!-- footer -->
 <div class="footer-area">
