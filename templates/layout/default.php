d242e024e9bf4f55ad852493f50db9da1604fecf
Yiming Huang <yhua0196@student.monash.edu>
2024-08-15 08:46:01 +1000

    fix the return button

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -119,17 +119,17 @@
                 $isAdminDashboardPage = $this->getRequest()->getParam('controller') === 'Pages' && $this->getRequest()->getParam('action') === 'display';
 
                 // Define the URL for the admin dashboard page
-                $adminDashboardUrl = $this->Url->build(['plugin' => null,'controller' => 'Pages', 'action' => 'admin_dashboard']);
+                $adminDashboardUrl = $this->Url->build(['plugin' => null,'controller' => 'adminDashboard', 'action' => 'dashboard']);
 
                 // Render the back button based on the page type
                 if(!$isAdminDashboardPage) {
                     if(!$isIndexPage) {
                         // If it's not an index page, render the back button
-//                        echo $this->Html->tag(
-//                            'button',
-//                            $this->Html->tag('i', '', ['class' => 'fas fa-arrow-left']) . ' Return',
-//                            ['onclick' => 'goBack()', 'class' => 'btn btn-secondary']
-//                        );
+                        echo $this->Html->tag(
+                            'button',
+                            $this->Html->tag('i', '', ['class' => 'fas fa-arrow-left']) . ' Return',
+                            ['onclick' => 'goBack()', 'class' => 'btn btn-secondary']
+                        );
                     } elseif($isIndexPage) {
                         // If it's an index page, return to the admin dashboard
                         echo $this->Html->tag(
