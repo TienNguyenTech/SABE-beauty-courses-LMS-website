5d2b5f7dc86c92310962313ccb735f2deb6ebfa6
Yiming Huang <yhua0196@student.monash.edu>
2024-08-14 20:21:33 +1000

    add controller

---
+++ src/Controller/AdminDashboardController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class AdminDashboardController extends AppController
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->viewBuilder()->setLayout('default');
+    }
+
+}
