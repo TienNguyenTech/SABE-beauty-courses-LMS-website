0ed9eefdc63d032078ae9b06a566d3df3ff61549
Yiming Huang <yhua0196@student.monash.edu>
2024-08-14 22:55:13 +1000

    update the logout button for admin page

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -158,36 +158,34 @@
                     }
                 </script>
 
+                <!-- Topbar Navbar -->
+
                 <ul class="navbar-nav ml-auto">
-
-                    <!-- Nav Item - User Information -->
-                    <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Username here</span>
-                            <?= $this->Html->image('undraw_profile.svg', ['class' => 'img-profile rounded-circle']) ?>
-                        </a>
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <!-- Logout link -->
-                            <?php
-                            if ($this->Identity->isLoggedIn()) {
-                                echo $this->Html->link(
-                                    'Logout',
-                                    [
-                                        'controller' => 'Auth',
-                                        'action' => 'logout',
-                                        '?' => ['redirect' => '/']
-                                    ],
-                                    ['class' => 'dropdown-item']
-                                );
-                            }
-                            ?>
-                        </div>
+                    <li class="nav-item">
+                        <?php if ($this->Identity->isLoggedIn()): ?>
+                            <a class="nav-link" href="#" style="color: black" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                        <?php endif; ?>
                     </li>
-
                 </ul>
 
+                <!-- Logout Confirmation Modal -->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" style="color: black;" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body" style="color: black;">Are you sure you want to logout?</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-secondary" type="button" data-dismiss="modal">No</button>
+                                <?= $this->Html->link('Yes', ['plugin' => null,'controller' => 'Auth', 'action' => 'logout'], ['class' => 'btn btn-primary']) ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </nav>
             <!-- End of Topbar -->
