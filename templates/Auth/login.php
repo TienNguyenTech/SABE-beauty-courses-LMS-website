11c108b4e1fc393306ac9f42b75fd3c31b3cb5b6
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-08-15 14:24:22 +1000

    Favicon

--- templates/Auth/login.php
+++ templates/Auth/login.php
@@ -13,6 +13,13 @@ $this->assign('title', 'Login');
 
 <html>
 
+<head>
+    <!-- Favicon-->
+    <link href="<?= $this->Url->build('/img/favicon.png') ?>" type="image/x-icon" rel="icon">
+    <!-- Including the shortcut icon ensures that all browsers, regardless of their version, will correctly find and use this favicon.  -->
+    <link href="<?= $this->Url->build('/img/favicon.png') ?>" type="image/x-icon" rel="shortcut icon">
+</head>
+
 <body>
     <style>
         body {
@@ -79,13 +86,13 @@ $this->assign('title', 'Login');
                         
                     </fieldset>
 
-<!--                    --><?php //= $this->Html->link('Forgotten your password?', ['controller' => 'Auth', 'action' => 'forgetPassword'], ['class' => 'button-clear']) ?>
-                    
+                    <!--                    --><?php //= $this->Html->link('Forgotten your password?', ['controller' => 'Auth', 'action' => 'forgetPassword'], ['class' => 'button-clear']) ?>
+
                     <div class="input-wrapper">
                         <?= $this->Form->button('Login', ['class' => 'centered-button']) ?>
                         <?= $this->Form->end() ?>
 
-                    </br>
+                        </br>
 
                         <?= $this->Html->link('Go to Homepage', '/', ['class' => 'button-clear']) ?>
                     </div>
