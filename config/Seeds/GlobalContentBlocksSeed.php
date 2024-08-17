12da77b4fc8986e98a78475615aaa4c8bfbbcff6
Yiming Huang <yhua0196@student.monash.edu>
2024-08-17 15:43:24 +1000

    updated seeds for content blocks

--- config/Seeds/GlobalContentBlocksSeed.php
+++ config/Seeds/GlobalContentBlocksSeed.php
@@ -9,7 +9,7 @@ class GlobalContentBlocksSeed extends AbstractSeed
     {
         $data = [
             [
-                'parent' => 'global',
+                'parent' => 'Website Essentials',
                 'label' => 'Website Title',
                 'description' => 'Shown on the home page, as well as any tabs in the users browser.',
                 'slug' => 'website-title',
@@ -17,19 +17,35 @@ class GlobalContentBlocksSeed extends AbstractSeed
                 'value' => 'South Adelaide Beauty and Education',
             ],
             [
-                'parent' => 'global',
+                'parent' => 'Website Essentials',
                 'label' => 'Logo',
                 'description' => 'Logo of your brand',
                 'slug' => 'logo',
                 'type' => 'image',
             ],
             [
-                'parent' => 'global',
+                'parent' => 'Website Essentials',
                 'label' => 'Copyright Message',
                 'description' => 'Copyright information shown at the bottom of the home page.',
                 'slug' => 'copyright-message',
                 'type' => 'text',
                 'value' => 'Copyrights © 2024, South Adelaide Beauty and Education.',
+            ],
+            [
+                'parent' => 'Website Essentials',
+                'label' => 'Contact Phone Number',
+                'description' => 'The contact phone number that shown on contact us page.',
+                'slug' => 'contact-phone',
+                'type' => 'text',
+                'value' => '0000000000',
+            ],
+            [
+                'parent' => 'Website Essentials',
+                'label' => 'Contact Email',
+                'description' => 'The contact email that shown on contact us page.',
+                'slug' => 'contact-email',
+                'type' => 'text',
+                'value' => 'beautybylisafollett@gmail.com',
             ],
         ];
 
