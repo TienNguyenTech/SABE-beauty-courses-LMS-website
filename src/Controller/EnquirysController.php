4a69e0c8b2ca1df64b9ae68c6a36a48d33859384
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-08-24 01:30:41 +1000

    Modified Contact Us Page

--- src/Controller/EnquirysController.php
+++ src/Controller/EnquirysController.php
@@ -52,7 +52,7 @@ class EnquirysController extends AppController
     public function add()
     {
         $this->viewBuilder()->disableAutoLayout();
-        $this->viewBuilder()->setLayout('contactUsLayout');
+        //$this->viewBuilder()->setLayout('contactUsLayout');
         $enquiry = $this->Enquirys->newEmptyEntity();
         if ($this->request->is('post')) {
             $enquiry = $this->Enquirys->patchEntity($enquiry, $this->request->getData());
