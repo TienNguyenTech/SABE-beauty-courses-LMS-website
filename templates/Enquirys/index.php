3a2a2d9f274e10fe1fb5bb812b0eff797ec04924
Yiming Huang <yhua0196@student.monash.edu>
2024-08-26 16:39:04 +1000

    Set a fixed length for the message section of the table

--- templates/Enquirys/index.php
+++ templates/Enquirys/index.php
@@ -7,6 +7,14 @@ echo $this->Html->css('/vendor/datatables/dataTables.bootstrap4.min.css', ['bloc
 echo $this->Html->script('/vendor/datatables/jquery.dataTables.min.js', ['block' => true]);
 echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['block' => true]);
 ?>
+<style>
+    .message {
+        max-width: 200px; /* Adjust this value to your needs */
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+</style>
 <div class="enquirys index content">
 <!--    --><?php //= $this->Html->link(__('New Enquiry'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Enquiries') ?></h3>
@@ -28,11 +36,13 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
                     <td><?= h($enquiry->enquiry_name) ?></td>
                     <td><?= h($enquiry->enquiry_email) ?></td>
                     <td><?= h($enquiry->enquiry_subject) ?></td>
-                    <td><?= h($enquiry->enquiry_message) ?></td>
+                    <td class="message" title="<?= h($enquiry->enquiry_message) ?>"><?= h($enquiry->enquiry_message) ?></td>
+<!--                    <td>--><?php //= h($enquiry->enquiry_message) ?><!--</td>-->
                     <td><?= h($enquiry->enquiry_seen ? 'Read' : 'Unread') ?></td>
                     <td class="actions">
 <!--                        --><?php //= $this->Html->link(__('View'), ['action' => 'view', $enquiry->enquiry_id]) ?>
 <!--                        --><?php //= $this->Html->link(__('Edit'), ['action' => 'edit', $enquiry->enquiry_id]) ?>
+                        <?= $this->Html->link('View Message', ['action' => 'viewMessage', $enquiry->enquiry_id], ['class' => 'btn btn-info']) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $enquiry->enquiry_id], ['class' => 'btn btn-danger', 'confirm' => __('Are you sure you want to delete enquiry: {0}?', $enquiry->enquiry_name)]) ?>
                         <?= $this->Html->link($enquiry->enquiry_seen ? 'Mark as Unread' : 'Mark as Read', ['action' => 'toggle', $enquiry->enquiry_id], ['class' => 'btn btn-primary']) ?>
                     </td>
@@ -44,6 +54,16 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
     <script>
         $(document).ready(function() {
             $('#dataTable').DataTable();
+        });
+        $(document).ready(function() {
+            $('.view-message').click(function() {
+                var message = $(this).data('message');
+                $('<div></div>').html(message).dialog({
+                    title: 'Message',
+                    width: 400,
+                    modal: true
+                });
+            });
         });
     </script>
 </div>
