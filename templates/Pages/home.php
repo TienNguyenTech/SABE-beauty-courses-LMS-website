4dd1cf3489cbc1ec9cc6bfdf3c18aa3da31a94f6
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-08-29 13:07:59 +1000

    Social Icons Link updated

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -927,17 +927,10 @@ if ($error = 404) {
                 <div class="col-lg-6 text-right col-md-12">
                     <div class="social-icons">
                         <ul>
-                            <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-                            <li><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+                            <li><a href="https://www.facebook.com/adelaidebeautyandeducation" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://www.instagram.com/adelaidebeautyandeducation" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.tiktok.com/@beautybylisafollett" target="_blank"><i class="fab fa-dribbble"></i></a></li>
                             <div class="site-logo">
-                            <?= $this->Html->link(
-                                $this->ContentBlock->image('logo'),
-                                '/',
-                                ['escape' => false]
-                            ) ?>
                         </div>
                         </ul>
                     </div>
