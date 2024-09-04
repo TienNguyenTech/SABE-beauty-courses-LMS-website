3f149c1b13ae38d713fd16ebdaca5d1a0c1f1b9e
Ben Hetherington <bhet0004@student.monash.edu>
2024-09-05 00:40:55 +1000

    Fix authentication issue
    fr this time?

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends AppController
 
         // By default, CakePHP will (sensibly) default to preventing users from accessing any actions on a controller.
         // These actions, however, are typically required for users who have not yet logged in.
-        $this->Authentication->allowUnauthenticated(['login', 'register', 'forgetPassword', 'changePassword']);
+        $this->Authentication->allowUnauthenticated(['login', 'register', 'forgetPassword', 'changePassword', 'resetPassword']);
 
         // CakePHP loads the model with the same name as the controller by default.
         // Since we don't have an Auth model, we'll need to load "Users" model when starting the controller manually.
