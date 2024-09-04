736824aa678412aeab944e9552dbf2943eb13f86
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-09-04 17:01:41 +1000

    shadow Course page

--- templates/Courses/courses.php
+++ templates/Courses/courses.php
@@ -290,6 +290,10 @@
         transform: translateY(0);
     }
 
+    .breadcrumb-text {
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+        }
+
 </style>
 
 <!-- breadcrumb-section -->
