e0ae175d9478d1e5c15aeb67dd46e66ef64e193e
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-09-16 01:55:45 +1000

    fIX THE public error

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -17,14 +17,14 @@ class CoursesController extends AppController
     private \Cake\ORM\Table $Quizzes;
 
     public function initialize(): void
-    {
-    {   
-        parent::initialize();
-        $this->Contents = TableRegistry::getTableLocator()->get("Contents");
-        $this->Quizzes = TableRegistry::getTableLocator()->get("Quizzes");
+    { {
+            parent::initialize();
+            $this->Contents = TableRegistry::getTableLocator()->get("Contents");
+            $this->Quizzes = TableRegistry::getTableLocator()->get("Quizzes");
 
-        // Controller-level function/action whitelist for authentication
-        $this->Authentication->allowUnauthenticated(['view', 'courses']);
+            // Controller-level function/action whitelist for authentication
+            $this->Authentication->allowUnauthenticated(['view', 'courses']);
+        }
     }
     public function courses()
     {
@@ -50,8 +50,8 @@ class CoursesController extends AppController
     }
 
     public function enrolledcourses()
-    {   
-        
+    {
+
         $query = $this->Courses->find();
         $courses = $this->paginate($query);
 
@@ -78,16 +78,17 @@ class CoursesController extends AppController
      * @return \Cake\Http\Response|null|void Renders view
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function course($id = null) {
+    public function course($id = null)
+    {
         $course = $this->Courses->get($id);
         $quiz = $this->Quizzes->find()->where(['course_id IS' => $course->course_id])->first();
 
-        if(!empty($quiz)) {
+        if (!empty($quiz)) {
             $quizID = $quiz->quiz_id;
             $quizJson = json_decode(json_decode($quiz->quiz_json));
             $quiz = $quizJson;
             $quiz->quiz_id = $quizID;
-        }        
+        }
 
         $query = $this->Contents->find()->where(['course_id IS' => $course->course_id]);
         $contents = $this->paginate($query);
@@ -96,16 +97,17 @@ class CoursesController extends AppController
     }
 
     /* Student accesses a course */
-    public function accesscourse($id = null) {
+    public function accesscourse($id = null)
+    {
         $course = $this->Courses->get($id);
         $quiz = $this->Quizzes->find()->where(['course_id IS' => $course->course_id])->first();
 
-        if(!empty($quiz)) {
+        if (!empty($quiz)) {
             $quizID = $quiz->quiz_id;
             $quizJson = json_decode(json_decode($quiz->quiz_json));
             $quiz = $quizJson;
             $quiz->quiz_id = $quizID;
-        }        
+        }
 
         $query = $this->Contents->find()->where(['course_id IS' => $course->course_id]);
         $contents = $this->paginate($query);
@@ -132,12 +134,12 @@ class CoursesController extends AppController
             // Check if the file size exceeds 10MB
             if ($image['course_image']->getSize() > 20 * 1024 * 1024) {
                 $this->Flash->error(__('Image file size must be 10MB or less.'));
-            } else if($image['course_image']->getClientMediaType() != 'image/jpeg' && $image['course_image']->getClientMediaType() != 'image/png') {
+            } else if ($image['course_image']->getClientMediaType() != 'image/jpeg' && $image['course_image']->getClientMediaType() != 'image/png') {
                 $this->Flash->error(__('The image must be either png or jpg format.'));
             } else {
                 $course->course_image = 'assets/img/products/' . $image['course_image']->getClientFilename();
 
-                if($this->Courses->save($course)) {
+                if ($this->Courses->save($course)) {
                     $image['course_image']->moveTo(WWW_ROOT . 'assets' . DS . 'img' . DS . 'products' . DS . $image['course_image']->getClientFilename());
                     $this->Flash->success(__('The course has been saved.'));
                     return $this->redirect(['action' => 'index']);
@@ -162,7 +164,7 @@ class CoursesController extends AppController
     public function edit($id = null)
     {
         $course = $this->Courses->get($id);
-        if($this->request->is(['patch', 'post', 'put'])) {
+        if ($this->request->is(['patch', 'post', 'put'])) {
 
             $image = $this->request->getUploadedFiles();
 
@@ -170,18 +172,18 @@ class CoursesController extends AppController
             $newCourse = $this->request->getData();
             $newCourse['course_image'] = $filename;
 
-            if($image['course_image']->getError() == \UPLOAD_ERR_NO_FILE) {
+            if ($image['course_image']->getError() == \UPLOAD_ERR_NO_FILE) {
                 $course = $this->Courses->patchEntity($course, $newCourse);
 
-                if($this->Courses->save($course)) {
+                if ($this->Courses->save($course)) {
                     $this->Flash->success(__('The course has been updated'));
-                    return $this->redirect(['action'=> 'index']);
+                    return $this->redirect(['action' => 'index']);
                 }
 
                 $this->Flash->error(__('The course could not be saved. Please try again.'));
-            } else if($image['course_image']->getSize() > 20 * 1024 * 1024) {
+            } else if ($image['course_image']->getSize() > 20 * 1024 * 1024) {
                 $this->Flash->error(__('Image file size must be 10MB or less.'));
-            } else if($image['course_image']->getClientMediaType() != 'image/jpeg' && $image['course_image']->getClientMediaType() != 'image/png') {
+            } else if ($image['course_image']->getClientMediaType() != 'image/jpeg' && $image['course_image']->getClientMediaType() != 'image/png') {
                 $this->Flash->error(__('The image must be either png or jpg format.'));
             } else {
                 $course = $this->Courses->patchEntity($course, $this->request->getData());
@@ -189,9 +191,9 @@ class CoursesController extends AppController
                 $course->course_image = 'assets/img/products/' . $image['course_image']->getClientFilename();
                 $image['course_image']->moveTo(WWW_ROOT . 'assets' . DS . 'img' . DS . 'products' . DS . $image['course_image']->getClientFilename());
 
-                if($this->Courses->save($course)) {
+                if ($this->Courses->save($course)) {
                     $this->Flash->success(__('The course has been updated'));
-                    return $this->redirect(['action'=> 'index']);
+                    return $this->redirect(['action' => 'index']);
                 }
 
                 $this->Flash->error(__('The course could not be saved. Please try again.'));
