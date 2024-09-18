168aa768c5a3a289a372bb983a410451ae130ec4
Ben Hetherington <bhet0004@student.monash.edu>
2024-09-18 11:16:47 +1000

    Fix formatting of payment confirmation

--- templates/email/html/payment_confirmation.php
+++ templates/email/html/payment_confirmation.php
@@ -2,6 +2,5 @@
     <h1>South Adelaide Beauty and Education</h1>
     <h3>Payment Confirmation Email</h3>
     <p>Hi <?= $name ?>,</p>
-    <p>Your payment for our <?= $courseName ?> course has been successful! <?= $this->Html->link('Click here', ['controller' => 'Courses', 'action' => 'accesscourse', $courseID]) ?></p>
-    <a href="<?= $courseURL ?>">Click here</a>
+    <p>Your payment for our <?= $courseName ?> course has been successful! <a href="<?= $courseURL ?>">Click here</a></p>
 </div>
