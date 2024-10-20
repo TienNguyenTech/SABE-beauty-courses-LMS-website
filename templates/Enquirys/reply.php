8ca14251047f266461c9d36a725aa61c14c88972
Ben Hetherington <bhet0004@student.monash.edu>
2024-10-20 20:48:09 +1100

    Create reply form

--- templates/Enquirys/reply.php
+++ templates/Enquirys/reply.php
@@ -28,10 +28,36 @@
             <tr>
                 <th><?= __('Actions') ?></th>
                 <td>
-                    <?= $this->Html->link('Reply', ['action' => 'reply', $enquiry->enquiry_id], ['class' => 'btn btn-primary']) ?>
+                    <?= $this->Html->link('View', ['action' => 'view', $enquiry->enquiry_id], ['class' => 'btn btn-primary']) ?>
                     <?= $this->Form->postLink('Archive', ['action' => 'archive', $enquiry->enquiry_id], ['class' => 'btn btn-danger archive-btn', 'confirm' => 'Are you sure you want to archive this enquiry?']) ?>
                 </td>
             </tr>
         </table>
+    </div>
+    <div class="enquirys view content">
+        <h3>Reply</h3>
+        <?= $this->Form->create() ?>
+        <table class="table">
+            <tr>
+                <th class="col-1"><?= __('Email') ?></th>
+                <td><?= $this->Form->control('reply_email', ['value' => $enquiry->enquiry_email, 'label' => false, 'style' => 'width: 600px', 'required']) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Email Subject') ?></th>
+                <td><?= $this->Form->control('reply_subject', ['style' => 'width: 600px', 'label' => false, 'required']) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Message') ?></th>
+                <td><?= $this->Form->textarea('reply_message', ['style' => 'width: 600px', 'label' => false, 'required']) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Actions') ?></th>
+                <td>
+                    <?= $this->Form->button('Send', ['value' => 'Submit', 'class' => 'btn btn-primary']) ?>
+                    <?= $this->Html->link('Cancel', ['action' => 'view', $enquiry->enquiry_id], ['class' => 'btn btn-danger']) ?>
+                </td>
+            </tr>
+        </table>
+        <?= $this->Form->end() ?>
     </div>
 </div>
