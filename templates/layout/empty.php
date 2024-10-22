3ef453ed0bfe525312b82fcd843c9801400e26c9
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-10-23 08:12:16 +1100

    Favicon on Login fixed

--- templates/layout/empty.php
+++ templates/layout/empty.php
@@ -1,7 +1,9 @@
 <html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?= $this->fetch('meta') ?> 
+    <!-- Favicon-->
+    <link href="<?= $this->Url->build('/img/favicon.png') ?>" type="image/x-icon" rel="icon">
+    <!-- Including the shortcut icon ensures that all browsers, regardless of their version, will correctly find and use this favicon.  -->
+    <link href="<?= $this->Url->build('/img/favicon.png') ?>" type="image/x-icon" rel="shortcut icon">
 </head>
 <body>
     <?= $this->fetch('content') ?>
